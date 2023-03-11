0b27d54e6623b9dcc11ddaee11ef3a2684e60d7b
Dylan Awalt-Conley <web@dylan.ac>
2023-03-10 19:16:54 -0500

    add javascript for spotlight buttons

---
+++ src/browser/spotlight-button.ts
@@ -0,0 +1,50 @@
+class SpotlightButton {
+  element: HTMLElement;
+  isAnimating: boolean = false;
+  constructor(e: HTMLElement) {
+    this.element = e;
+    this.onMouseMove = this.onMouseMove.bind(this);
+
+    e.addEventListener('mouseenter', () => this.addListener(), {
+      passive: true,
+    });
+    e.addEventListener('mouseleave', () => this.removeListener(), {
+      passive: true,
+    });
+  }
+
+  onMouseMove({ offsetX, offsetY }: MouseEvent): void {
+    if (this.isAnimating) return;
+    this.isAnimating = true;
+    const adjustX = offsetX - this.element.clientWidth / 2;
+    const adjustY = offsetY - this.element.clientHeight / 2;
+    requestAnimationFrame(() => {
+      this.element.style.setProperty(
+        '--translate',
+        `${(-adjustX / 4).toFixed(2)}px, ${-adjustY / 4}px`
+      );
+      this.isAnimating = false;
+    });
+  }
+
+  addListener(): void {
+    this.isAnimating = false;
+    this.element.addEventListener('mousemove', this.onMouseMove);
+  }
+
+  removeListener(): void {
+    this.element.removeEventListener('mousemove', this.onMouseMove);
+  }
+}
+
+const buttons = document.getElementsByClassName(
+  'spotlight-button'
+) as HTMLCollectionOf<HTMLElement>;
+
+const spotlightButtons: SpotlightButton[] = [];
+for (const button of buttons) {
+  const sp = new SpotlightButton(button);
+  spotlightButtons.push(sp);
+}
+
+export { spotlightButtons as buttons };
