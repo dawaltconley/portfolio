ed468f836c2d1b8a5a676476e3c52929f29f5612
Dylan Awalt-Conley <web@dylan.ac>
2023-03-12 14:11:08 -0400

    fix getMetadata using eleventy types

--- src/utils.ts
+++ src/utils.ts
@@ -1,21 +1,22 @@
-import { createHash } from 'node:crypto';
-import EleventyFetch from '@11ty/eleventy-fetch';
-import MetaScraper from 'metascraper';
+import EleventyFetch, { AssetCache } from '@11ty/eleventy-fetch';
+import MetaScraper, { Metadata } from 'metascraper';
 import MetaScraperImage from 'metascraper-image';
 
 const ms = MetaScraper([MetaScraperImage()]);
 
-const hash = (str: string) => createHash('md5').update(str).digest('hex');
-
-const getMetadata = async (url: URL, duration = '1w') => {
+const getMetadata = async (url: URL, duration = '1w'): Promise<Metadata> => {
   let html = await EleventyFetch(url.href, {
     duration,
     type: 'text',
-  }).catch((e: any) => {
-    console.warn(`${e.code}: unable to fetch data from ${url}`);
-    return '';
+  }).catch((e: Error) => {
+    if ('code' in e) {
+      console.warn(`${e.code}: unable to fetch data from ${url}`);
+      return '';
+    } else {
+      throw e;
+    }
   });
-  let cache = new EleventyFetch.AssetCache(hash(html));
+  let cache = new AssetCache<Metadata>(html);
   if (cache.isCacheValid(duration)) {
     return cache.getCachedValue();
   }
@@ -25,8 +26,11 @@ const getMetadata = async (url: URL, duration = '1w') => {
     return metadata;
   } catch (e) {
     console.warn(`Unable to scrape metadata from ${url}:`);
-    console.warn(e);
-    return cache.isCacheValid('*') ? cache.getCachedValue() : {};
+    if (cache.isCacheValid('*')) {
+      console.warn(e);
+      return cache.getCachedValue();
+    }
+    throw e;
   }
 };
 
