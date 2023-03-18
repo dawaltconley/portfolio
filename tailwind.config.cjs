7eb40b6c299182e9e018d2db8a725c63cb2bacf8
Dylan Awalt-Conley <web@dylan.ac>
2023-03-18 15:39:18 -0400

    add tailwing selector for dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+  darkMode: ['class', '[data-color-scheme="dark"]'],
   theme: {
     container: {
       padding: {
