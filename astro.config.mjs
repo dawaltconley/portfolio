cb062034be4d86ab98e0f942a5caa1609349cf5e
Dylan Awalt-Conley <web@dylan.ac>
2023-03-25 15:35:21 -0400

    add unplugin-icons with fontawesome pro libraries

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,48 @@
 import { defineConfig } from 'astro/config';
 import path from 'node:path';
+import preact from '@astrojs/preact';
 import sass from 'sass';
 import mkTailwindFunctions from 'sass-tailwind-functions';
-import preact from '@astrojs/preact';
+import Icons from 'unplugin-icons/vite';
+import { FileSystemIconLoader } from 'unplugin-icons/loaders';
 
 const tailwindFunctions = mkTailwindFunctions(
   sass,
   path.resolve('./tailwind.config.cjs')
 );
 
+const fapSvgPath = (type = '') =>
+  path.join('node_modules', '@fortawesome', 'fontawesome-pro', 'svgs', type);
+const fapSvgCallback = (svg) =>
+  svg.replace(/^<svg /, '<svg fill="currentColor" ');
+
+const fapCollection = Object.fromEntries(
+  [
+    'brands',
+    'duotone',
+    'light',
+    'regular',
+    'sharp-regular',
+    'sharp-solid',
+    'solid',
+    'thin',
+  ].map((type) => [
+    `fap-${type}`,
+    FileSystemIconLoader(fapSvgPath(type), fapSvgCallback),
+  ])
+);
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
   vite: {
+    plugins: [
+      Icons({
+        compiler: 'jsx',
+        jsx: 'preact',
+        customCollections: fapCollection,
+      }),
+    ],
     css: {
       preprocessorOptions: {
         scss: {
