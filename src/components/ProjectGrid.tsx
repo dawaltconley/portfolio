01dde3e7a3a27c3caa28e9fdd46708c027a4684a
Dylan Awalt-Conley <web@dylan.ac>
2023-03-29 18:21:34 -0400

    add filters for all tags to bottom of the page

--- src/components/ProjectGrid.tsx
+++ src/components/ProjectGrid.tsx
@@ -1,28 +1,68 @@
-import type { FunctionComponent } from 'preact';
+import type { FunctionComponent, ComponentProps } from 'preact';
 import ProjectPreview, { ProjectPreviewProps } from './ProjectPreview';
-import { useState, useEffect } from 'preact/hooks';
+import { useState, useEffect, useMemo } from 'preact/hooks';
 
 const ProjectFilter: FunctionComponent<{
   handleFilter: (tag: string) => void;
   tag: string;
   active?: string[];
-}> = ({ tag, active = [], handleFilter, children }) => {
+  style?: 'tab' | 'link';
+}> = ({ tag, style = 'link', active = [], handleFilter, children }) => {
   const isActive = active.includes(tag);
+  const props: ComponentProps<'button'> = useMemo(() => {
+    if (style === 'tab')
+      return {
+        class: `spotlight-button spotlight-button--no-js block overflow-hidden border-l-2 border-theme-tx bg-theme-bg p-4 font-medium uppercase leading-none transition-all duration-300 last:border-r-2 ${
+          isActive ? 'bg-theme-br text-white' : ''
+        }`,
+        style: {
+          '--scale': 1.5,
+          ...(isActive
+            ? { '--opacity': 0.3, '--color': 'var(--theme-bg)' }
+            : {}),
+        },
+      };
+    return {
+      class: `text-xs ${isActive ? 'font-semibold' : 'text-theme-tx/80'}`,
+    };
+  }, [style, isActive]);
   return (
-    <button
-      class={`spotlight-button spotlight-button--no-js block overflow-hidden border-l-2 border-theme-tx bg-theme-bg p-4 font-medium uppercase leading-none transition-all duration-300 last:border-r-2 ${
-        isActive ? 'bg-theme-br text-white' : ''
-      }`}
-      style={{
-        '--scale': 1.5,
-        ...(isActive ? { '--opacity': 0.3, '--color': 'var(--theme-bg)' } : {}),
-      }}
-      onClick={() => handleFilter(tag)}
-    >
+    <button {...props} onClick={() => handleFilter(tag)}>
+      {style === 'link' && (isActive ? '[-] ' : '[+] ')}
       {children}
     </button>
   );
 };
+
+const tagLabels = new Map([
+  ['website', 'website'],
+  ['11ty', 'Eleventy'],
+  ['sass', 'Sass'],
+  ['nunjucks', 'Nunjucks'],
+  ['npm', 'NPM'],
+  ['parser', 'parser'],
+  ['javascript', 'JavaScript'],
+  ['app', 'app'],
+  ['cli', 'command line'],
+  ['node', 'Node.js'],
+  ['typescript', 'TypeScript'],
+  ['react', 'React'],
+  ['nextjs', 'Next.js'],
+  ['tailwind', 'Tailwind CSS'],
+  ['aws', 'Amazon Web Services'],
+  ['cloudformation', 'CloudFormation'],
+  ['component', 'component'],
+  ['jekyll', 'Jekyll'],
+  ['liquid', 'Liquid'],
+  ['audio', 'audio'],
+]);
+
+interface TagData {
+  [tag: string]: {
+    label: string;
+    count: number;
+  };
+}
 
 interface ProjectPreviewData extends ProjectPreviewProps {
   tags: string[];
@@ -36,6 +76,21 @@ const ProjectGrid: FunctionComponent<{
   const [filter, setFilter] = useState(
     ([] as string[]).concat(initFilter ?? [])
   );
+
+  const tags: TagData = useMemo(() => {
+    const tagData: TagData = {};
+    projects.forEach(({ tags }) =>
+      tags.forEach((t) => {
+        if (t in tagData) {
+          tagData[t].count++; // actually can do this with a Map as well
+        } else {
+          const label = tagLabels.get(t) ?? t;
+          tagData[t] = { count: 1, label };
+        }
+      })
+    );
+    return tagData;
+  }, [projects]);
 
   const updateSearchParams = (newFilter: typeof filter): void => {
     const url = new URL(window.location.href);
@@ -61,23 +116,34 @@ const ProjectGrid: FunctionComponent<{
 
   return (
     <>
-      <div class="flex justify-center border-t-2 border-theme-tx">
-        {/* <p class="block p-4 font-medium uppercase leading-none">Portfolio:</p> */}
+      <nav class="flex justify-center border-t-2 border-theme-tx">
+        <p class="block p-4 font-medium uppercase leading-none">Portfolio:</p>
         <ProjectFilter
           tag="website"
+          style="tab"
           active={filter}
           handleFilter={handleFilter}
         >
           Websites
         </ProjectFilter>
-        <ProjectFilter tag="app" active={filter} handleFilter={handleFilter}>
+        <ProjectFilter
+          tag="app"
+          style="tab"
+          active={filter}
+          handleFilter={handleFilter}
+        >
           Apps
         </ProjectFilter>
-        <ProjectFilter tag="npm" active={filter} handleFilter={handleFilter}>
+        <ProjectFilter
+          tag="npm"
+          style="tab"
+          active={filter}
+          handleFilter={handleFilter}
+        >
           Packages
         </ProjectFilter>
-      </div>
-      <ul class="grid gap-4 py-8 sm:grid-cols-2 xl:grid-cols-3">
+      </nav>
+      <ul class="my-8 grid gap-4 sm:grid-cols-2 xl:grid-cols-3">
         {projects
           .filter(({ tags }) =>
             filter.length ? filter.some((f) => tags.includes(f)) : true
@@ -88,6 +154,19 @@ const ProjectGrid: FunctionComponent<{
             </ProjectPreview>
           ))}
       </ul>
+      <hr class="my-2 border-theme-tx/10" />
+      <nav class="space-x-3 text-center leading-tight">
+        {Object.entries(tags).map(([tag, { label, count }], i) => (
+          <>
+            {/* i > 0 && <span class="text-sm">–</span> */}
+            <ProjectFilter
+              tag={tag}
+              active={filter}
+              handleFilter={handleFilter}
+            >{`${label} (${count})`}</ProjectFilter>
+          </>
+        ))}
+      </nav>
     </>
   );
 };
