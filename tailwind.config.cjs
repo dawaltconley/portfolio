2d6349dbea95f18da8c2cacfe789c1aee097e562
Dylan Awalt-Conley <web@dylan.ac>
2023-04-04 16:33:38 -0400

    add tailwind utility for min-aspect-ratio

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
   darkMode: ['class', '[data-color-scheme="dark"]'],
@@ -13,5 +15,42 @@ module.exports = {
       serif: ['Erode-Variable', 'Erode', 'serif'],
     },
   },
-  plugins: [require('sass-themes/tailwind')],
+  plugins: [
+    require('sass-themes/tailwind'),
+    plugin(({ matchUtilities, theme }) => {
+      matchUtilities(
+        {
+          'min-aspect': (value) => ({
+            '&::before': {
+              content: "''",
+              paddingTop: value,
+              float: 'left',
+            },
+            '&::after': {
+              content: "''",
+              display: 'table',
+              clear: 'both',
+            },
+          }),
+        },
+        {
+          values: Object.entries(theme('aspectRatio')).reduce(
+            (values, [k, aspect]) => {
+              let n = Number(aspect);
+              if (Number.isNaN(n)) {
+                const [x, y] = aspect.split('/').map(Number);
+                n = y / x;
+              }
+              if (Number.isNaN(n)) return values;
+              return {
+                ...values,
+                [k]: (100 * n).toFixed(2) + '%',
+              };
+            },
+            {}
+          ),
+        }
+      );
+    }),
+  ],
 };
