3194f168a673838e017dfbf8b20c30cfb02cfebf
Dylan Awalt-Conley <web@dylan.ac>
2023-04-13 12:49:45 -0400

    add function for resolving file paths in project frontmatter

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,7 +1,18 @@
 import type { CollectionEntry } from 'astro:content';
 import { getCollection } from 'astro:content';
+import fsp from 'node:fs/promises';
+import path from 'node:path';
 import { marked } from 'marked';
 import { compile } from 'html-to-text';
+
+const fileExists = (file: string): Promise<boolean> =>
+  fsp
+    .stat(file)
+    .then(() => true)
+    .catch((e) => {
+      if (e.code === 'ENOENT') return false;
+      else throw e;
+    });
 
 const projects = await getCollection('projects');
 
@@ -40,4 +51,18 @@ const getExcerpt = (
   return convertExcerpt(marked.parse(excerpt));
 };
 
-export { projects, tags, getExcerpt };
+const searchPaths: string[] = [
+  path.resolve('.'),
+  path.resolve('./src/content/projects'),
+  path.resolve('./src/assets/images'),
+];
+
+const resolvePath = async (file: string): Promise<string> => {
+  for (let p of searchPaths) {
+    const resolved = path.resolve(p, file);
+    if (await fileExists(resolved)) return resolved;
+  }
+  throw new Error(`Couldn't resolve path for project file: ${file}`);
+};
+
+export { projects, tags, getExcerpt, resolvePath };
